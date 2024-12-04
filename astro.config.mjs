5947b0df0c16df3039e73901057b2bcca36a8467
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-12-04 19:53:33 +0000

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,33 +5,37 @@ import fs from 'fs-extra';
 
 const siteUrl = 'https://graphics-for-good.com';
 
-const directoryPath = path.join('./', 'members');
-const members = fs.readdirSync(directoryPath);
+const mPath = path.join('./', 'members');
+const cPath = path.join('./', 'content');
+const members = fs.readdirSync(mPath);
 
 members.forEach((username, i) => {
-    var isDir = fs.lstatSync(`${directoryPath}/${username}`).isDirectory()
+    var isDir = fs.lstatSync(`${mPath}/${username}`).isDirectory()
     if (isDir) {
         var portfolio = []
-        if (fs.existsSync(`${directoryPath}/${username}/portfolio/`)) {
-            if (fs.lstatSync(`${directoryPath}/${username}/portfolio/`).isDirectory()) {
-                var items = fs.readdirSync(`${directoryPath}/${username}/portfolio/`)
-                items.forEach(i => {
-                    portfolio.push({
-                        file: `${username}-${i}`
-                    })
-                    if (!fs.existsSync(`${directoryPath}/${username}/renamedPortfolio/`)) {
-                        fs.mkdirSync(`${directoryPath}/${username}/renamedPortfolio/`)
+        if (fs.existsSync(`${mPath}/${username}/portfolio/`)) {
+            if (fs.lstatSync(`${mPath}/${username}/portfolio/`).isDirectory()) {
+                var items = fs.readdirSync(`${mPath}/${username}/portfolio/`)
+                items.forEach(item => {
+                    var obj = {
+                        username: username,
+                        file: item
                     }
-                    if (fs.existsSync(`${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)) {
-                        fs.unlinkSync(`${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)
+                    portfolio.push(obj)
+                    if (!fs.existsSync(`${mPath}/${username}/portfolio/${getFNameNoExt(item)}/`)) {
+                        fs.mkdirSync(`${mPath}/${username}/portfolio/${getFNameNoExt(item)}/`)
                     }
-                    fs.copySync(`${directoryPath}/${username}/portfolio/${i}`, `${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)
+                    fs.copySync(`${mPath}/${username}/portfolio/${item}`, `${mPath}/${username}/portfolio/${getFNameNoExt(file)}/${item}`)
+
+                    obj.src = 'info.js'
+                    obj = `var data = ${JSON.stringify(obj, null, 4)}\n\nexport default data`
+                    fs.writeFileSync(`${mPath}/${username}/portfolio/${getFNameNoExt(file)}/info.js`)
                 })
             }
         }
 
 
-        var info = `${directoryPath}/${username}/info.json`
+        var info = `${mPath}/${username}/info.json`
         if (fs.existsSync(info)) {
             info = fs.readFileSync(info, 'utf-8')
             if (info) {
@@ -42,12 +46,40 @@ members.forEach((username, i) => {
                     
                     info = JSON.stringify(info, null, 2)
                     info = `var data = ${info}\n\nexport default data`
-                    fs.writeFileSync( `${directoryPath}/${username}/info.js`, info)
+                    fs.writeFileSync( `${mPath}/${username}/info.js`, info)
                 }
             }
         }
     }
 });
+
+function getFName(str) {
+    if (str.includes('/')) str = str.split('/').slice(-1)[0]
+    return str
+}
+
+function getFNameNoExt(str) {
+  if (str.includes('/')) str = str.split('/').slice(-1)[0]
+  str = str.slice(0, -1*getFExt(str, 2).length)
+  return str
+}
+
+function getFExt(str, level=1) {
+  str = getFName(str)
+  if (level > 0) level *= -1
+  if (str.includes('.')) {
+    str = str.split('.')
+    switch (level) {
+      case 0: 
+        break
+      default: 
+        str = str.slice(level)
+        break
+    }
+    str = str.join('.')
+  }
+  return str
+}
 
 export default defineConfig({
     site: siteUrl,
