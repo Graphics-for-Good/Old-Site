7696f53c399b56546aef8754767e8c1996a1f114
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-12-04 18:22:55 +0000

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,46 +8,46 @@ const siteUrl = 'https://graphics-for-good.com';
 const directoryPath = path.join('./', 'members');
 const members = fs.readdirSync(directoryPath);
 
-members.forEach((username, i) => {
-    var isDir = fs.lstatSync(`${directoryPath}/${username}`).isDirectory()
-    if (isDir) {
-        var portfolio = []
-        if (fs.existsSync(`${directoryPath}/${username}/portfolio/`)) {
-            if (fs.lstatSync(`${directoryPath}/${username}/portfolio/`).isDirectory()) {
-                var items = fs.readdirSync(`${directoryPath}/${username}/portfolio/`)
-                items.forEach(i => {
-                    portfolio.push({
-                        file: `${username}-${i}`
-                    })
-                    if (!fs.existsSync(`${directoryPath}/${username}/renamedPortfolio/`)) {
-                        fs.mkdirSync(`${directoryPath}/${username}/renamedPortfolio/`)
-                    }
-                    if (fs.existsSync(`${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)) {
-                        fs.unlinkSync(`${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)
-                    }
-                    fs.copySync(`${directoryPath}/${username}/portfolio/${i}`, `${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)
-                })
-            }
-        }
+// members.forEach((username, i) => {
+//     var isDir = fs.lstatSync(`${directoryPath}/${username}`).isDirectory()
+//     if (isDir) {
+//         var portfolio = []
+//         if (fs.existsSync(`${directoryPath}/${username}/portfolio/`)) {
+//             if (fs.lstatSync(`${directoryPath}/${username}/portfolio/`).isDirectory()) {
+//                 var items = fs.readdirSync(`${directoryPath}/${username}/portfolio/`)
+//                 items.forEach(i => {
+//                     portfolio.push({
+//                         file: `${username}-${i}`
+//                     })
+//                     if (!fs.existsSync(`${directoryPath}/${username}/renamedPortfolio/`)) {
+//                         fs.mkdirSync(`${directoryPath}/${username}/renamedPortfolio/`)
+//                     }
+//                     if (fs.existsSync(`${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)) {
+//                         fs.unlinkSync(`${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)
+//                     }
+//                     fs.copySync(`${directoryPath}/${username}/portfolio/${i}`, `${directoryPath}/${username}/renamedPortfolio/${username}-${i}`)
+//                 })
+//             }
+//         }
 
 
-        var info = `${directoryPath}/${username}/info.json`
-        if (fs.existsSync(info)) {
-            info = fs.readFileSync(info, 'utf-8')
-            if (info) {
-                if (info.startsWith('{') && info.endsWith('}')) {
-                    info = JSON.parse(info)
-                    info.username = username
-                    info.portfolio = portfolio
+//         var info = `${directoryPath}/${username}/info.json`
+//         if (fs.existsSync(info)) {
+//             info = fs.readFileSync(info, 'utf-8')
+//             if (info) {
+//                 if (info.startsWith('{') && info.endsWith('}')) {
+//                     info = JSON.parse(info)
+//                     info.username = username
+//                     info.portfolio = portfolio
                     
-                    info = JSON.stringify(info, null, 2)
-                    info = `var data = ${info}\n\nexport default data`
-                    fs.writeFileSync( `${directoryPath}/${username}/info.js`, info)
-                }
-            }
-        }
-    }
-});
+//                     info = JSON.stringify(info, null, 2)
+//                     info = `var data = ${info}\n\nexport default data`
+//                     fs.writeFileSync( `${directoryPath}/${username}/info.js`, info)
+//                 }
+//             }
+//         }
+//     }
+// });
 
 export default defineConfig({
     site: siteUrl,
